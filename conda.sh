e6e6e2784560b2f5cdff0e93438360e05d79a0a8
Mina Gaid <mina-gaid@users.noreply.github.com>
2018-08-28 10:30:32 +0100

    Create conda.sh

---
+++ conda.sh
@@ -0,0 +1,12 @@
+conda install anaconda
+
+# Stable environment -
+# Updates all packages to latest 'releases', that work well together
+
+conda update conda
+conda update anaconda
+
+# Unstable environment -
+# Updates all packages to latest 'releases'
+
+conda update --all
